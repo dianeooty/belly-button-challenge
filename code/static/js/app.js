779465b3fbf2138f985afa9a1a95b718fc3e9c9c
Diane Guzman <117790100+dianeooty@users.noreply.github.com>
2023-03-20 12:58:15 -0700

    Updates: Code

--- code/static/js/app.js
+++ code/static/js/app.js
@@ -27,12 +27,12 @@ d3.json(url).then(function (data) {
       .property("value", sampleNames[i]);
   };
 
-    // Call the functions to display the selected test subject ID's demographic info and plots
-    buildMetadata(sampleNames[0]);
-    buildCharts(sampleNames[0]);
-  });
+  // Call the functions to display the selected test subject ID's demographic info and plots
+  buildMetadata(sampleNames[0]);
+  buildCharts(sampleNames[0]);
+});
 
-  // Create a function to extract the metadata for the demographic info table
+// Create a function to extract the metadata for the demographic info table
 function buildMetadata(sampleId) {
   d3.json(url).then(function (data) {
     var metaData = data.metadata;
@@ -67,4 +67,77 @@ function optionChanged(sampleId) {
 };
 
 // Create a function to plot the bar and bubble charts 
-function buildCharts(sampleValues)
+function buildCharts(sampleValues) {
+  d3.json(url).then(function (data) {
+    var samples = data.samples;
+    console.log("Extracted Samples:", data.samples);
+    
+    // Filter by ID
+    let resultArray = samples.filter(sampleObj => sampleObj.id == sampleValues);
+    let result = resultArray[0];
+    console.log("Samples:", result);
+
+    // Assign variables for the OTU IDs, OTU labels and sample_values
+    let otu_ids = result.otu_ids;
+    let otu_labels = result.otu_labels;
+    let sample_values = result.sample_values;
+
+    // Check the values for each variable
+    console.log("OTU IDs:", otu_ids, "OTU Labels:", otu_labels, "Sample_Values:", sample_values)
+
+    // Set y ticks for top ten OTU 
+    let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+
+    // Check the values for yticks
+    console.log("Yticks:", yticks)
+
+    // Set data for bar chart
+    let barData = [
+      {
+        y: yticks,
+        x: sample_values.slice(0, 10).reverse(),
+        text: otu_labels.slice(0, 10).reverse(),
+        type: "bar",
+        orientation: "h",
+      }
+    ];
+
+    // Set layout for bar chart
+    let barLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      margin: { t: 30, l: 150 }
+    };
+
+    // Plot bar chart with barData and barLayout
+    Plotly.newPlot("bar", barData, barLayout);
+
+    // Set layout for bubble chart
+    let bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      margin: { t: 0 },
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" },
+      margin: { t: 30 }
+    };
+
+    // Set data for bubble chart
+    let bubbleData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: "Earth"
+        }
+      }
+    ];
+
+    // Plot bubble chart with bubbleData and bubbleLayout
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+  })
+
+};
